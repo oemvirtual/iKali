59d25e7c02a09a171787f7e504142a11a8cacea7
Cyber-Gen <bzabeti@uh.edu>
2022-01-28 03:33:32 -0600

    updated Readme + added Licenses & project maker script

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # iKali
 Script installer for useful tools in kali
+
+1. Running "./project-templates.sh" will create the template folder's in the current user's home directory
+
+
+Future changes:
+1. Add auto installer for the common tools
+    Toolbox
+         1. Nmap + zenmap
+         2. Ghidra
+         3. Burpsuite
+         4. ZAP
+         5. xsser
+         6. Maltego
+         7. King Phisher
+         8. vscode
+         9. timeshift
+        10. snap
+        11. docker
+        12. bitwarden
+        13. nordvpn
+        14. metasploit
---
+++ project-templates.sh
@@ -0,0 +1,30 @@
+#! /bin/bash
+
+# go to current user's home directory 
+cd $HOME
+
+# creating template 1
+mkdir -p Project-Templates/Template-1/pre-engagement
+mkdir -p Project-Templates/Template-1/reports
+mkdir -p Project-Templates/Template-1/results
+mkdir -p Project-Templates/Template-1/linux/exploitation
+mkdir -p Project-Templates/Template-1/linux/info-gathering
+mkdir -p Project-Templates/Template-1/linux/lateral-movement
+mkdir -p Project-Templates/Template-1/linux/post-exploitation
+mkdir -p Project-Templates/Template-1/linux/vulnerability-assessment
+mkdir -p Project-Templates/Template-1/windows/exploitation
+mkdir -p Project-Templates/Template-1/windows/info-gathering
+mkdir -p Project-Templates/Template-1/windows/lateral-movement
+mkdir -p Project-Templates/Template-1/windows/post-exploitation
+mkdir -p Project-Templates/Template-1/windows/vulnerability-assessment
+# creating template 2
+mkdir -p Project-Templates/Template-2/pre-engagement
+mkdir -p Project-Templates/Template-2/reports
+mkdir -p Project-Templates/Template-2/results
+mkdir -p Project-Templates/Template-2/social-engineering
+mkdir -p Project-Templates/Template-2/webapp-pentesting
+mkdir -p Project-Templates/Template-2/network-pentesting/exploitation
+mkdir -p Project-Templates/Template-2/network-pentesting/info-gathering
+mkdir -p Project-Templates/Template-2/network-pentesting/lateral-movement
+mkdir -p Project-Templates/Template-2/network-pentesting/post-exploitation
+mkdir -p Project-Templates/Template-2/network-pentesting/vulnerability-assessment
